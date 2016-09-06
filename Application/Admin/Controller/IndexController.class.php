e2eff971b332bd29328b459cfcfa5c103b8dfb5f
lph <wirelessqa.me@gmail.com>
2016-09-06 14:04:21 +0800

    no message

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/9/6
+ * Time: 1:02
+ */
+
+namespace Admin\Controller;
+use Think\Controller;
+
+class IndexController extends Controller{
+
+    public function index(){
+        $this->display();
+    }
+}
